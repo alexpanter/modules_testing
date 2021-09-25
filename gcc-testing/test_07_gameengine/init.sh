2f1775ef7e25f61edd4ae39fe5d8667a44ca3989
alexpanter <alex_c007@hotmail.com>
2021-09-25 22:31:48 +0200

    Way better build system for test gameengine

--- gcc-testing/test_07_gameengine/init.sh
+++ gcc-testing/test_07_gameengine/init.sh
@@ -1,1 +1,13 @@
 mkdir -p gcm.cache
+
+# remove gcc module caches in case they were added by mistake
+rm -rf engine/gcm.cache/
+rm -rf testgame/gcm.cache/
+
+# create symbolic links so that all projects use the same module cache
+CURDIR=$(pwd)
+cd engine/
+ln -fs ../gcm.cache gcm.cache
+
+cd ../testgame/
+ln -fs ../gcm.cache gcm.cache
