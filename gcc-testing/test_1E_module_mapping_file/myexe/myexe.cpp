3df8350a79e716f9fcfded34803ae82ad60a0eed
alexpanter <alex_c007@hotmail.com>
2021-11-06 18:42:04 +0100

    Mapping file with module partition

--- gcc-testing/test_1E_module_mapping_file/myexe/myexe.cpp
+++ gcc-testing/test_1E_module_mapping_file/myexe/myexe.cpp
@@ -5,6 +5,7 @@ import mylib;
 int main()
 {
 	std::cout << "get_value(): " << get_value() << '\n';
+	std::cout << "get_part(): " << get_part() << '\n';
 
 	return 0;
 }
--- gcc-testing/test_1E_module_mapping_file/mylib/mylib.cpp
+++ gcc-testing/test_1E_module_mapping_file/mylib/mylib.cpp
@@ -1,5 +1,7 @@
 export module mylib;
 
+export import :part;
+
 export int get_value()
 {
 	return 42;
