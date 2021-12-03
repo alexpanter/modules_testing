47fa0345f1f34fcc5656a33cf9fbbc15f9e44cd0
Nic <acavano@outlook.com>
2021-12-03 19:26:00 +0200

    Clang 13 installed thru HomeBrew Example (tested on M1 processor)

---
+++ clang-testing/test1-cpp20/main.cpp
@@ -0,0 +1,9 @@
+import <iostream>;
+import mathlib;
+
+using namespace std;
+
+int main() {
+	cout << "Modules, baby!" << endl;
+	cout << "2 plus 3 makes " << add(2, 3) << " says module 'mathlib'" << endl;
+}
